89e0fd3c6442cff907272e7e5c65ef4b4583aa56
Babita-Bhatia29 <bhatiababita296@gmail.com>
2026-07-01 22:17:32 +0530

    frontend-ml-api and local testing

--- app.py
+++ app.py
@@ -1,33 +1,63 @@
-from flask import Flask,request,jsonify
+from flask import Flask,request,jsonify,render_template
 import pandas as pd
 import pickle
 
 app=Flask(__name__)
 
 
+
+def get_cleaned_data(form_data):
+    gestation= float(form_data['gestation'])
+    parity= int(form_data['parity'])
+    age= float(form_data['age'])
+    height= float(form_data['height'])
+    weight= float(form_data['weight'])
+    smoke= float(form_data['smoke'])
+
+
+    cleaned_data={"gestation": [gestation],
+                   "parity": [parity],
+                   "age": [age],
+                    "height": [height],
+                   "weight": [weight],
+                   "smoke": [smoke]
+                   }
+    return cleaned_data
+
+#creating home page
+@app.route("/",methods=["GET"])
+def home():
+    #get data from user
+
+    return render_template("index.html")
+
+
 ##define Your endpoint here
 @app.route("/predict",methods=["POST"])
 def get_prediction():
     #get data From user
-    baby_data=request.get_json()
+    baby_data_form=request.form
+
+    baby_data_cleaned = get_cleaned_data(baby_data_form)
+
 
     #convert into data frame
-    baby_df=pd.DataFrame(baby_data) 
+    baby_df=pd.DataFrame(baby_data_cleaned) 
 
     #load machine learning trained model
-    with open("model/model.pkl","rb")as f:
+    with open("model/model.pkl","rb")as f: 
         mymodel=pickle.load(f)
     
 
     #making prediction on user data
     prediction=mymodel.predict(baby_df)
     
-    prediction=round(float(prediction),2)
+    prediction=round(float(prediction[0]),2)
 
 
 
-    response={"prediction":prediction}
-    return jsonify(response)
+    response={"Prediction":prediction}
+    return render_template("index.html",prediction=prediction)
 
 
 
